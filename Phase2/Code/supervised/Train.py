1eb8c873791a2b4faad7f5773507770a3d03decf
Satyapalsinh Gohil <125583562+satyapalsinh10@users.noreply.github.com>
2024-01-11 09:24:44 -0500

    Train.py

--- Phase2/Code/supervised/Train.py
+++ Phase2/Code/supervised/Train.py
@@ -1,16 +1,5 @@
 #!/usr/bin/env python
 
-"""
-RBE/CS Fall 2022: Classical and Deep Learning Approaches for
-Geometric Computer Vision
-Project 1: MyAutoPano: Phase 2 Starter Code
-
-
-Author(s):
-Lening Li (lli4@wpi.edu)
-Teaching Assistant in Robotics Engineering,
-Worcester Polytechnic Institute
-"""
 
 from ast import Pass
 from email.mime import image
