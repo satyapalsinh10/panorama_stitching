40bb433013ce24f84bc745deb466ad630ef3ac43
Satyapalsinh Gohil <125583562+satyapalsinh10@users.noreply.github.com>
2024-01-11 09:24:13 -0500

    Test.py

--- Phase2/Code/supervised/Test.py
+++ Phase2/Code/supervised/Test.py
@@ -4,11 +4,6 @@ RBE/CS Fall 2022: Classical and Deep Learning Approaches for
 Geometric Computer Vision
 Project 1: MyAutoPano: Phase 2 Starter Code
 
-
-Author(s):
-Lening Li (lli4@wpi.edu)
-Teaching Assistant in Robotics Engineering,
-Worcester Polytechnic Institute
 """
 
 
